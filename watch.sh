dd8b9cb219c9d4a8320846116ffc5ab535cc6e9f
Albert Dahlin <info@albertdahlin.com>
2022-12-11 18:43:00 +0100

    Don't run on compile error

--- watch.sh
+++ watch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 while true; do
-inotifywait -q -e close_write src/* test/* input/*
-echo -e "\n\e[32m*** WATCH ***\e[0m\n"
-make $1
+    inotifywait -q -e close_write src/* test/* input/*
+    echo -e "\n\e[32m*** WATCH ***\e[0m\n"
+    make -j4 $1
 
-if [ ! $1 = "test" ]; then
-  build/$1 $2 $3 $4 $5 $6
-fi
+    if [ $? -gt 0 ]; then
+        continue
+    fi
+
+    if [ ! $1 = "test" ]; then
+      build/$1 $2 $3 $4 $5 $6
+    fi
 done
